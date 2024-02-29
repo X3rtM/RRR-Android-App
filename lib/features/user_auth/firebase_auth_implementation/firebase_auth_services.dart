7282a515d2122b3ce86d5309fc33d8ebdea312e5
Sairaj <nsairaj36@gmail.com>
2024-02-29 11:39:01 +0530

    Updated code

--- lib/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart
+++ lib/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart
@@ -1,23 +1,15 @@
-
-
-
-
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../../global/common/toast.dart';
 
-
 class FirebaseAuthService {
-
   FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<User?> signUpWithEmailAndPassword(String email, String password) async {
-
     try {
-      UserCredential credential =await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       return credential.user;
     } on FirebaseAuthException catch (e) {
-
       if (e.code == 'email-already-in-use') {
         showToast(message: 'The email address is already in use.');
       } else {
@@ -25,13 +17,15 @@ class FirebaseAuthService {
       }
     }
     return null;
+  }
 
+  Future<void> signOut() async {
+    await _auth.signOut();
   }
 
   Future<User?> signInWithEmailAndPassword(String email, String password) async {
-
     try {
-      UserCredential credential =await _auth.signInWithEmailAndPassword(email: email, password: password);
+      UserCredential credential = await _auth.signInWithEmailAndPassword(email: email, password: password);
       return credential.user;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found' || e.code == 'wrong-password') {
@@ -39,15 +33,7 @@ class FirebaseAuthService {
       } else {
         showToast(message: 'An error occurred: ${e.code}');
       }
-
     }
     return null;
-
   }
-
-
-
-
 }
-
-
--- lib/features/user_auth/presentation/pages/login_page.dart
+++ lib/features/user_auth/presentation/pages/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +6,11 @@ import 'package:flutter_firebase/features/user_auth/presentation/widgets/form_co
 import 'package:flutter_firebase/global/common/toast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../firebase_auth_implementation/firebase_auth_services.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -23,12 +22,23 @@ class _LoginPageState extends State<LoginPage> {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  bool _rememberMe = false;
+
+  late SharedPreferences _preferences;
 
   @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    _initializeSharedPreferences();
+  }
+
+  void _initializeSharedPreferences() async {
+    _preferences = await SharedPreferences.getInstance();
+    setState(() {
+      _emailController.text = _preferences.getString('email') ?? '';
+      _passwordController.text = _preferences.getString('password') ?? '';
+      _rememberMe = _preferences.getBool('rememberMe') ?? false;
+    });
   }
 
   @override
@@ -38,121 +48,161 @@ class _LoginPageState extends State<LoginPage> {
         automaticallyImplyLeading: false,
         title: Text("Login"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 550),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Login",
-                style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              FormContainerWidget(
-                controller: _emailController,
-                hintText: "Email",
-                isPasswordField: false,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              FormContainerWidget(
-                controller: _passwordController,
-                hintText: "Password",
-                isPasswordField: true,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GestureDetector(
-                onTap: () {
-                  _signIn();
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 45,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: _isSigning ? CircularProgressIndicator(
-                      color: Colors.white,) : Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("img/login_bckgnd.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-              ),
-              SizedBox(height: 10,),
-              GestureDetector(
-                onTap: () {
-                  _signInWithGoogle();
-
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 45,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(10),
+                  SizedBox(height: 30),
+                  FormContainerWidget(
+                    controller: _emailController,
+                    hintText: "Email",
+                    isPasswordField: false,
+                    onFieldSubmitted: (_) => _signIn(),
                   ),
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(FontAwesomeIcons.google, color: Colors.white,),
-                        SizedBox(width: 5,),
-                        Text(
-                          "Sign in with Google",
+                  SizedBox(height: 10),
+                  FormContainerWidget(
+                    controller: _passwordController,
+                    hintText: "Password",
+                    isPasswordField: true,
+                    onFieldSubmitted: (_) => _signIn(),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: _rememberMe,
+                        onChanged: (value) {
+                          setState(() {
+                            _rememberMe = value!;
+                            _preferences.setBool('rememberMe', value);
+                          });
+                        },
+                      ),
+                      Text('Remember Me'),
+                      Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          // Implement forgot password functionality
+                          _forgotPassword();
+                        },
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      _signIn();
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: _isSigning
+                            ? CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                            : Text(
+                          "Login",
                           style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-
-
-              SizedBox(
-                height: 20,
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have an account?"),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()),
-                            (route) => false,
-                      );
-                    },
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
+                  SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () {
+                      _signInWithGoogle();
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.google,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "Sign in with Google",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(width: 5),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => SignUpPage()),
+                                (route) => false,
+                          );
+                        },
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
@@ -167,48 +217,111 @@ class _LoginPageState extends State<LoginPage> {
     String email = _emailController.text;
     String password = _passwordController.text;
 
-    User? user = await _auth.signInWithEmailAndPassword(email, password);
+    try {
+      User? user = await _auth.signInWithEmailAndPassword(email, password);
 
+      setState(() {
+        _isSigning = false;
+      });
+
+      if (user != null) {
+        showToast(message: "User is successfully signed in");
+        Navigator.pushNamed(context, "/home");
+      } else {
+        showToast(message: "Some error occurred");
+      }
+    } catch (e) {
+      showToast(message: "Failed to sign in: $e");
+    }
+  }
+
+  void _signInWithGoogle() async {
     setState(() {
-      _isSigning = false;
+      _isSigning = true;
     });
 
-    if (user != null) {
-      showToast(message: "User is successfully signed in");
-      Navigator.pushNamed(context, "/home");
-    } else {
-      showToast(message: "some error occured");
-    }
-  }
-
-
-  _signInWithGoogle()async{
-
-    final GoogleSignIn _googleSignIn = GoogleSignIn();
-
     try {
+      final GoogleSignIn googleSignIn = GoogleSignIn(
+        clientId: '204717000834-7gqfsabhohifacopumr9mnkkbot8l3j2.apps.googleusercontent.com',
+      );
 
-      final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
 
-      if(googleSignInAccount != null ){
-        final GoogleSignInAuthentication googleSignInAuthentication = await
-        googleSignInAccount.authentication;
-
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
         final AuthCredential credential = GoogleAuthProvider.credential(
-          idToken: googleSignInAuthentication.idToken,
           accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
         );
 
+        final UserCredential authResult =
         await _firebaseAuth.signInWithCredential(credential);
-        Navigator.pushNamed(context, "/home");
+        final User? user = authResult.user;
+
+        setState(() {
+          _isSigning = false;
+        });
+
+        if (user != null) {
+          showToast(message: "User is successfully signed in");
+          Navigator.pushNamed(context, "/home");
+        } else {
+          showToast(message: "Some error occurred");
+        }
+      } else {
+        showToast(message: "Google Sign-In cancelled");
+        setState(() {
+          _isSigning = false;
+        });
       }
+    } catch (error) {
+      print("Google Sign-In Error: $error");
+      String errorMessage = "Error occurred during Google Sign-In";
 
-    }catch(e) {
-showToast(message: "some error occured $e");
+      if (error is FirebaseAuthException) {
+        switch (error.code) {
+          case 'account-exists-with-different-credential':
+            errorMessage =
+            "An account already exists with the same email address but different sign-in credentials.";
+            break;
+          case 'invalid-credential':
+            errorMessage = "The credential received is malformed or has expired.";
+            break;
+          case 'operation-not-allowed':
+            errorMessage =
+            "Google Sign-In is currently not enabled. Please contact support.";
+            break;
+          case 'user-disabled':
+            errorMessage = "The user account has been disabled by an administrator.";
+            break;
+          case 'user-not-found':
+            errorMessage =
+            "There is no user corresponding to the given Google sign-in credentials.";
+            break;
+          case 'wrong-password':
+            errorMessage = "Invalid password provided for the Google account.";
+            break;
+          default:
+            errorMessage = "An unknown error occurred. Please try again later.";
+            break;
+        }
+      }
+      showToast(message: errorMessage);
+      setState(() {
+        _isSigning = false;
+      });
     }
-
-
   }
 
-
+  void _forgotPassword() async {
+    String email = _emailController.text;
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+      showToast(message: "Password reset email sent to $email");
+    } catch (error) {
+      print("Forgot Password Error: $error");
+      showToast(message: "Failed to send password reset email: $error");
+    }
+  }
 }
